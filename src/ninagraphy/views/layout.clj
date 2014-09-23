ef3ef94630e262fba369898f39e3031223c89f07
Oleg Neizhkasha <panshdw@gmail.com>
2014-09-24 00:28:28 +0300

    Default layout with react and km-script

--- src/ninagraphy/views/layout.clj
+++ src/ninagraphy/views/layout.clj
@@ -1,9 +1,14 @@
 (ns ninagraphy.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css include-js]]))
 
 (defn common [& body]
   (html5
     [:head
-     [:title "Welcome to ninagraphy"]
+     [:title "Welcome to ninagraphy:"]
+     (include-js "http://fb.me/react-0.11.2.js")
+
      (include-css "/css/screen.css")]
-    [:body body]))
+    [:body
+       body
+       (include-js "/js/om.ninagraphy.js")
+     ]))
