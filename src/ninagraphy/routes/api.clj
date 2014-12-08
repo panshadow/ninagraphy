88dd309251eea286415f2eaf2646ef69f4344727
Oleg Neizhkasha <panshdw@gmail.com>
2014-12-09 00:16:01 +0200

    Updated API

--- src/ninagraphy/routes/api.clj
+++ src/ninagraphy/routes/api.clj
@@ -5,24 +5,43 @@
 (def VERSION "v1")
 (def api (str  "/api/" VERSION))
 
+(def photos [
+             {:album 1 :title "one" :photo "/photos/IMG_0632.JPG"}
+             {:album 2 :title "two" :photo "/photos/IMG_8108.JPG"}
+             {:album 2 :title "three" :photo "/photos/IMG_8155.JPG"}  ])
+
 (defn api-info []
   (str "NinaGraphy API " VERSION))
 
 
-(defn list-albums [req]
-  {:status 200
-   :body {
-          :albums [
-                   {:id 1
-                    :title "Якась назва"},
-                   {:id 2
-                    :title ""}]}})
 
-(defn get-album [req]
-  {:status 404
-   :body {}})
+(defn get-album
+  ([] (let [albums (group-by :album photos)
+            ids (keys albums)]
+        (map (fn [id]
+               {:id id
+                :photos (get albums id [])
+                }) ids)))
+  
+  ([id] {:id id
+         :photos (filter #(-> % :album (str) (= id)) photos)
+         }))
+
+
+(defn list-albums [req]
+  (let [albums (get-album)]
+    {:status 200
+     :body {:albums albums}}))
+
+(defn one-album [req id]
+  (let [album (get-album id)]
+    {:status 200
+     :body {:albums [album]}}
+    ))
+
+
 
 (defroutes api-routes
   (GET (str api "/") [] (api-info))
   (GET (str api "/albums") [] list-albums )
-  (GET (str api "/albums/:album") [] get-album))
+  (GET (str api "/albums/:album") [] one-album))
